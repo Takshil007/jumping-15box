42879805f2cd1eb1a53a5d05a8dd5177c0a562b1
Takshil007 <76619707+Takshil007@users.noreply.github.com>
2021-06-19 10:24:11 +0530

    Add files via upload

---
+++ paper.js
@@ -0,0 +1,12 @@
+var option = {
+    isStatic : false,
+    restitution : 0.3,
+    friction : 0,
+    density : 1.2   
+}
+function image()   {
+this.image = loadImage("paper.png");
+display() 
+}  
+ 
+
